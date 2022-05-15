e73c0f97cfea480cfa17dcedbd428a7802033806
Sam Knopper <samanthaknopper@hotmail.com>
2022-05-15 13:09:24 +0800

    seeds, models completed, sequilize working, api testing started

--- models/index.js
+++ models/index.js
@@ -5,13 +5,21 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category);
 // Categories have many Products
-
+Category.hasMany(Product);
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+  as: "product_tags"
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product,{
+  through: ProductTag,
+  foreignKey: 'tag_id',
+  as: "product_tags"
+});
 module.exports = {
   Product,
   Category,
