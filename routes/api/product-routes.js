e73f1803b50cd4d92c3e1cbd74a6937b2e085058
Sam Knopper <samanthaknopper@hotmail.com>
2022-05-15 17:46:00 +0800

    routes completed except update for tags and products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -81,10 +81,17 @@ router.post('/', (req, res) => {
 // update product
 router.put('/:id', (req, res) => {
   // update product data
-  Product.update(req.body, {
+  Product.update({
+      name: req.body.name,
+      price: req.body.price,
+      stock: req.body.stock,
+      category_id: req.body.category_id,
+      tag_id: req.body.tagIds,
+    }, {
     where: {
       id: req.params.id,
-    },
+    }
+  
   })
     .then((product) => {
       // find all associated tags from ProductTag
